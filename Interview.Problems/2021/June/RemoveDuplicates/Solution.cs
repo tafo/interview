7739c84cb1530eda7501cc7ed1c7a4e32a09591d
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-06-28 18:06:21 +0300

    RemoveDuplicates - Add - Solution

---
+++ Interview.Problems/2021/June/RemoveDuplicates/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Interview.Problems._2021.June.RemoveDuplicates
+{
+    public class Solution
+    {
+        public string RemoveDuplicates(string s)
+        {
+            var letterStack = new Stack<char>();
+            letterStack.Push(s[^1]);
+            for (var i = s.Length - 2; i >= 0; i--)
+            {
+                if (letterStack.TryPeek(out var pre) && pre == s[i])
+                {
+                    letterStack.Pop();
+                }
+                else
+                {
+                    letterStack.Push(s[i]);
+                }
+            }
+
+            return string.Concat(letterStack);
+        }
+    }
+}
---
+++ Interview.Problems/2021/June/RemoveDuplicates/Test.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Interview.Problems._2021.June.RemoveDuplicates
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(string input, string output)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.RemoveDuplicates(input);
+            timer.Stop();
+            actualOutput.Should().Be(output);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                "abbaca",
+                "ca"
+            },
+            new object[]
+            {
+                "azxxzy",
+                "ay"
+            }
+        };
+    }
+}
