a77ebaf8b38f05120611f6cf5a75e62a43363d6b
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-07-03 16:22:42 +0300

    FindClosestElements - Add - Solution

---
+++ Interview.Problems/2021/July/FindClosestElements/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interview.Problems._2021.July.FindClosestElements
+{
+    public class Solution
+    {
+        public IList<int> FindClosestElements(int[] arr, int k, int x)
+        {
+            return null;
+        }
+
+        public IList<int> FindClosestElementsLinq(int[] arr, int k, int x)
+        {
+            return arr
+                .Select(num => new Number {Original = num, Difference = Math.Abs(num - x)})
+                .OrderBy(n => n.Difference)
+                .Take(k).Select(n => n.Original).OrderBy(n => n).ToList();
+        }
+    }
+
+    public class Number
+    {
+        public int Difference { get; set; }
+        public int Original { get; set; }
+    }
+}
---
+++ Interview.Problems/2021/July/FindClosestElements/Test.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Interview.Problems._2021.July.FindClosestElements
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[] input, int k, int x, IList<int> output)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.FindClosestElements(input, k, x);
+            timer.Stop();
+            actualOutput.Should().BeEquivalentTo(output);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[] {1,2,3,4,5},
+                4,
+                3,
+                new[] {1,2,3,4}.ToList()
+            },
+            new object[]
+            {
+                new[] {1,2,3,4,5},
+                4,
+                -1,
+                new[] {1,2,3,4}.ToList()
+            },
+        };
+    }
+}
