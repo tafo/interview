e51378f28dc3f3d74c952d600d9425d82817a2fb
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-07-03 18:53:56 +0300

    MaxSumSubmatrix - Add - Solution

---
+++ Interview.Problems/2021/July/MaxSumSubMatrix/Solution.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Interview.Problems._2021.July.MaxSumSubMatrix
+{
+    public class Solution
+    {
+        public int MaxSumSubmatrix(int[][] matrix, int k)
+        {
+            var currentMax = MaxSubArraySum(matrix[0]);
+            if (matrix.Length == 1)
+            {
+                return currentMax;
+            }
+
+            var output = currentMax;
+
+            for (var i = 0; i < matrix.Length - 1; i++)
+            {
+                var summingArray = matrix[i];
+                output = Math.Max(output, MaxSubArraySum(summingArray));
+                for (var j = i + 1; j < matrix.Length; j++)
+                {
+                    summingArray = summingArray.Zip(matrix[j], (x, y) => x + y).ToArray();
+                    currentMax = MaxSubArraySum(summingArray);
+                    output = Math.Max(output, currentMax);
+                }
+            }
+
+            return output;
+
+            int MaxSubArraySum(IReadOnlyList<int> arr)
+            {
+                var result = arr[0];
+                var max = arr[0];
+                for (var i = 1; i < arr.Count; i++)
+                {
+                    max = Math.Max(arr[i], max + arr[i]);
+                    if (max <= k)
+                    {
+                        result = Math.Max(result, max);
+                    }
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ Interview.Problems/2021/July/MaxSumSubMatrix/Test.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Interview.Problems._2021.July.MaxSumSubMatrix
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[][] input, int k, int output)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.MaxSumSubmatrix(input, k);
+            timer.Stop();
+            actualOutput.Should().Be(output);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[] {new[] {1, 0, 1}, new[] {0, -2, 3}},
+                2,
+                2,
+            },
+            new object[]
+            {
+                new[] {new[] {2, 2, -1}},
+                3,
+                3,
+            },
+            new object[]
+            {
+                new[] {new[] {2, 2, -1}},
+                0,
+                -1,
+            },
+            new object[]
+            {
+                new[] {new[] {4, 4, 2}},
+                3,
+                2,
+            },
+            new object[]
+            {
+                new[]
+                {
+                    new[] {2, 1, -3, -4, 5}, new[] {0, 6, 3, 4, 1}, new[] {2, -2, -1, 4, -5}, new[] {-3, 3, 1, 0, 3}
+                },
+                30,
+                18,
+            },
+        };
+    }
+}
