824191ec2ada478e137be75f6e92be36659299eb
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-07-07 07:31:24 +0300

    MaxSumSubmatrix - Add - SolutionAnother

---
+++ Interview.Benchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace Interview.Benchmark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.ReadLine();
+            BenchmarkRunner.Run(typeof(Problems._2021.July.MaxSumSubMatrix.Benchmark));
+            Console.ReadLine();
+        }
+    }
+}
--- Interview.Problems/2021/July/MaxSumSubMatrix/SolutionBruteForce.cs
+++ Interview.Problems/2021/July/MaxSumSubMatrix/SolutionBruteForce.cs
@@ -1,31 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Interview.Problems._2021.July.MaxSumSubMatrix
 {
-    public class Solution
+    public class SolutionBruteForce
     {
         public int MaxSumSubmatrix(int[][] matrix, int k)
         {
-            var output = new List<int>();
+            var sumList = new List<int>();
 
             for (var i = 0; i < matrix.Length; i++)
             {
                 var rectangle = matrix[i];
-                output.AddRange(GetSumListOfRectangles(rectangle));
+                sumList.AddRange(GetSumListOfRectangles(rectangle));
                 for (var j = i + 1; j < matrix.Length; j++)
                 {
                     rectangle = rectangle.Zip(matrix[j], (x, y) => x + y).ToArray();
-                    output.AddRange(GetSumListOfRectangles(rectangle));
+                    sumList.AddRange(GetSumListOfRectangles(rectangle));
                 }
             }
 
-            var filteredList = output.Where(x => x <= k).ToList();
+            return sumList.Where(x => x <= k).Max();
 
-            return filteredList.Max();
-
-            static IEnumerable<int> GetSumListOfRectangles(IReadOnlyList<int> arr)
+            IEnumerable<int> GetSumListOfRectangles(IReadOnlyList<int> arr)
             {
                 var resultList = new List<int>();
                 for (var i = 0; i < arr.Count; i++)
